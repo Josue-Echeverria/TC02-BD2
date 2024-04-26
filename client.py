db49484d314c12d7f4ce4698214346388663e91b
Alevdelgado <110568267+Alevdelgado@users.noreply.github.com>
2024-04-25 19:15:12 -0600

    client.py  (add channel-mongo)

--- client.py
+++ client.py
@@ -2,12 +2,18 @@ import threading
 import os
 from consumer import consumer
 from producer import producer, send_data
+from db_mongo import MongoDB
+
 KAFKA1 = os.getenv('KAFKA_BROKER1')
 TOPIC = os.getenv('TOPIC')
 
 if __name__ == "__main__":
     client_name = input("What is your name? ")
     topic_name = input("What topic you want to subscribe? ")
+    
+    # envia nuevo topic a mongodb
+    channel = MongoDB().add_channel(topic_name)
+    print(channel)
 
     send_data(client_name,KAFKA1,topic_name, "\n"+client_name + " has entered the room\n")
     background_thread = threading.Thread(target=consumer, args=(KAFKA1,topic_name))
